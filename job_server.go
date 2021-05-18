b18fe2ab513d165ba642e99f5d4917bf887df8e7
James <newmanjt15@gmail.com>
2021-05-18 13:03:26 -0500

    async

--- job_server.go
+++ job_server.go
@@ -163,3 +163,62 @@ func LoadConfig(config_file string) Config {
 	common.CheckError(err)
 	return cd
 }
+
+func AsyncGetResults(job_id string, engine string, topic string, num int, thumb_size string, rec_chan chan JobRequest) {
+	fmt.Println(fmt.Sprintf("Getting %s on %s for %s", topic, engine, job_id))
+	js := GetJSString(engine)
+
+	res := chrome_server.Search(fmt.Sprintf("https://%s", GetSearchString(engine, topic)), js)
+
+	fmt.Println(fmt.Sprintf("%s for %s %s", res, job_id, engine))
+	r := res.([]interface{})
+
+	c := 0
+	done_chan := make(chan int)
+	// pause_chan := make(chan bool)
+	for i, l := range r {
+		fmt.Println(fmt.Sprintf("%d: %s %s", i, l, engine))
+		if l == nil {
+			continue
+		}
+		if strings.Contains(l.(string), "duckduck") {
+			continue
+		}
+		if c == num {
+			break
+		}
+
+		l_str := l.(string)
+		if strings.Contains(l_str, ":~:text=") {
+			l_str = l_str[:strings.Index(l_str, "#")]
+		}
+		// if i > 0 {
+		// 	<-pause_chan
+		// }
+		go func(i int) {
+			x := make(chan interface{})
+			go GetWebPage(l_str, thumb_size, x)
+			select {
+			case loc := <-x:
+				loca := loc.(chrome_server.JSEval)
+				fmt.Println("update for ", engine, topic)
+				rec_chan <- JobRequest{ID: job_id, Type: "update", List: []SearchResult{SearchResult{URL: l_str, Location: loca.Loc, LoadTime: loca.Res.DomComplete - loca.Res.NavigationStart, SpeedIndex: loca.Res.SpeedIndex, FirstPaint: loca.Res.FirstPaint, SearchEngine: engine, Images: loca.Res.Images, Words: loca.Res.Words, Scripts: loca.Res.Scripts}}}
+				// pause_chan <- true
+				done_chan <- i
+			case <-time.Tick(time.Second * 30):
+				rec_chan <- JobRequest{ID: job_id, Type: "update", List: []SearchResult{SearchResult{URL: l_str, Location: "null", SearchEngine: engine}}}
+				done_chan <- i
+				// pause_chan <- true
+			}
+		}(c)
+		c += 1
+	}
+	for {
+		select {
+		case z := <-done_chan:
+			if z == num {
+				return
+			}
+		}
+	}
+}
